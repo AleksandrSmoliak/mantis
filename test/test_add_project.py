c6de1577bf415c6d0fb1ab15120cceb4d864b81c
AleksandrSmoliak <crucis.spb@gmail.com>
2018-07-16 13:30:48 +0300

    Добавлено предусловие для теста удаления проекта.

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,9 +1,15 @@
 from model.project import Project
 
 def test_add_project(app):
+    # Переходим на страницу с проектами
     app.project.open_project_page()
+    # Получакем количество созданных проектов
     old_project = app.project.project_count()
+    # Добавляем новый проект
     app.project.create_project(Project(name="Проект1", description="Описание проекта"))
+    # Возвращаемся на страницу с проектами
     app.project.open_project_page()
+    # Получаем новое значение количества проектов
     new_project = app.project.project_count()
+    # Проверяем что количество изменилось
     assert old_project + 1 == new_project
--- test/test_del_project.py
+++ test/test_del_project.py
@@ -1,8 +1,20 @@
 from random import randrange
+from model.project import Project
+
+
 def test_del_project(app):
+    # открываем страницуц с проектами
     app.project.open_project_page()
+    # Получаем количествво созданных проектов
     old_project = app.project.project_count()
+    # Реализуем предусловие
+    if old_project == 0:
+        app.project.create_project(Project(name="Проект1", description="Описание проекта"))
+    # Герерируем случайный индекс с ограничением по количеству существующих проектов
     index = randrange(0, old_project)
+    # Удаляем проект по индексу
     app.project.del_project_by_index(index)
+    # Получаем новое значение количества проектов
     new_project = app.project.project_count()
+    # Проверяем что количество проектов изменилось
     assert old_project - 1 == new_project
